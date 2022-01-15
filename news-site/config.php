5061ef1171c3b52992656a8bdb0150aad74debd1
dharmapra2 <dharmapra865@gmail.com>
2022-01-15 21:21:42 +0530

    commit

--- news-site/config.php
+++ news-site/config.php
@@ -1,4 +1,4 @@
 <?php
 $hostName= "https://localhost/news-site";
-$conn=mysqli_connect("localhost:3307","root","","news-site") or die("Connection failed..". mysqli_connect_error());
+$conn=mysqli_connect("localhost:3306","root","","news-site") or die("Connection failed..". mysqli_connect_error());
 ?>
